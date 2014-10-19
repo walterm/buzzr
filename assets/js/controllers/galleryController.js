44f3828863c6c9ee4ad57ea69730f400e74c8bf3
Walter Menendez <walterm@mit.edu>
2014-10-19 05:57:56 -0400

    Hack

--- assets/js/controllers/galleryController.js
+++ assets/js/controllers/galleryController.js
@@ -1,5 +1,12 @@
 var app = angular.module("buzzrApp", []);
-$('#myGallery').spacegallery({loadingClass: 'loading'});
+
+var buzzes = []
+// Setting up leap motion controller
+var controllerOptions = {enableGestures: true};
+var time = new Date().getTime(),
+    threshold = 500,
+    loopcontroller = new Leap.Controller(controllerOptions);
+loopcontroller.connect();
 
 app.controller("galleryController", function ($scope) {
     var cur_time = new Date();
@@ -8,16 +15,13 @@ app.controller("galleryController", function ($scope) {
     $scope.header = "";
     $scope.likes = [];
     prev_time.setDate(prev_time.getDate() - 1);
-    // Setting up leap motion controller
-    var controllerOptions = {enableGestures: true};
-    var time = new Date().getTime(),
-        threshold = 500,
-        loopcontroller = new Leap.Controller(controllerOptions);
-    loopcontroller.connect();
+
 
     loopcontroller.on("gesture", function(gesture){
         if(gesture.type === "swipe"){
-            if($("#myGallery img")[$("myGallery img").length-1].attr("data-done") == "true") {
+            var leng = $("#myGallery img").length;
+            if(!$scope.buzzes) {
+                console.dir($($("#myGallery img")[leng - 1]).attr("data-done"));
                 $("#myGallery").html("You are done! Wait 24 hours.");
             }
 
@@ -25,23 +29,27 @@ app.controller("galleryController", function ($scope) {
             if(now - time > 500){
                 if(gesture.direction[0] > 0) {
                     // right case
-                    $scope.likes.push(scope.buzzes.pop())
+                    console.dir("swipe right");
+                    $scope.likes.push($scope.buzzes.pop());
                 } else {
                     // left case
+                    console.dir("swipe left");
                     $scope.buzzes.pop();
                 }
-                $scope.header = $scope.buzzes[$scope.buzzes.length].title;
-                $("#myGallery img")[$("myGallery img").length-1].css('display', 'none').attr('data-done', 'true');
-                $("#myGallery a").click();
+                time = now;
+                $scope.header = $scope.buzzes[$scope.buzzes.length-1].title;
+                $scope.$apply();
             }
         }
     });
     $.ajax({
-        url: "www.buzfeed/buzfeed/api/buzzes?since=" + prev_time.getTime() + "&until=" + cur_time.getTime() + "& session_key=4621030e12e085226f2cba99d0b19f1a2cc1337b9c7ed8fd91668ec97d1e844ehackathon3",
+        url: "http://www.buzzfeed.com/buzzfeed/api/buzzes?since=" + Math.floor(prev_time.getTime() / 1000 )+ "&until=" + Math.floor(cur_time.getTime() / 1000) + "&session_key=4621030e12e085226f2cba99d0b19f1a2cc1337b9c7ed8fd91668ec97d1e844ehackathon3",
         async: false,
         dataiType: "json"
     }).done( function (e) {
-        console.dir(e);
-        $scope.buzzes.push(e.buzzes)
+        $scope.buzzes.push(e.buzzes);
+        $scope.buzzes = $scope.buzzes[0];
+        $scope.header = $scope.buzzes[$scope.buzzes.length-1].title
+        buzzes = $scope.buzzes;
     });
 });
