ccfd57c783de8c26ee98b835cd80d3084c484168
Kyle Holzinger <kylelholzinger@gmail.com>
2014-10-19 03:54:19 -0400

    IC

---
+++ assets/js/controllers/galleryController.js
@@ -0,0 +1,14 @@
+var app = angular.module("buzzrApp", []);
+var buzzes = [];
+app.controller("galleryController", function ($scope) {
+    var cur_time = new Date();
+    var prev_time = new Date();
+    prev_time.setDate(prev_time.getDate() - 1);
+    $.ajax({
+        url: "www.buzfeed/buzfeed/api/buzzes?since=" + prev_time.getTime() + "&until=" + cur_time.getTime() + "& session_key=4621030e12e085226f2cba99d0b19f1a2cc1337b9c7ed8fd91668ec97d1e844ehackathon3",
+        async: false,
+        dataiType: "json"
+    }).done( function (e) {
+        console.dir(e);
+    });
+});
