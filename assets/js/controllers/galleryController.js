4f8d2c5db3421363f546d0989fa6aaf9823c1a0f
Kyle Holzinger <kylelholzinger@gmail.com>
2014-10-19 04:03:34 -0400

    Integrate AngularJS

--- assets/js/controllers/galleryController.js
+++ assets/js/controllers/galleryController.js
@@ -1,8 +1,9 @@
 var app = angular.module("buzzrApp", []);
-var buzzes = [];
+
 app.controller("galleryController", function ($scope) {
     var cur_time = new Date();
     var prev_time = new Date();
+    $scope.buzzes = []
     prev_time.setDate(prev_time.getDate() - 1);
     $.ajax({
         url: "www.buzfeed/buzfeed/api/buzzes?since=" + prev_time.getTime() + "&until=" + cur_time.getTime() + "& session_key=4621030e12e085226f2cba99d0b19f1a2cc1337b9c7ed8fd91668ec97d1e844ehackathon3",
@@ -10,5 +11,6 @@ app.controller("galleryController", function ($scope) {
         dataiType: "json"
     }).done( function (e) {
         console.dir(e);
+        $scope.buzzes.push(e.buzzes)
     });
 });
